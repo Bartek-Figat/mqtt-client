ef0ce9795e1efe95e2b466617cb33b698273fa54
Bartek-Figat <figat29@gmail.com>
2021-10-23 00:40:47 -0400

    mqtt-client live streaming data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
+import "chartjs-adapter-luxon";
 import { Chart, registerables } from "chart.js";
-import DataLabelsPlugin from "chartjs-plugin-datalabels";
-import StreamingPlugin from "chartjs-plugin-streaming";
+import { StreamingPlugin, RealTimeScale } from "chartjs-plugin-streaming";
 import moment from "moment";
 
 const socket = io("ws://localhost:8000", {
@@ -10,116 +10,122 @@ const socket = io("ws://localhost:8000", {
   secure: true,
 });
 
-Chart.register(...registerables, DataLabelsPlugin, StreamingPlugin);
-const currentTime = moment(new Date(), "HH:mm:ss").format("HH:mm:ss");
+Chart.register(...registerables, StreamingPlugin, RealTimeScale);
 function App() {
   const canvas = useRef();
-  const increment = useRef();
-
   let dataChart;
+  let currentLevel;
 
-  useEffect(() => {
-    socket.on("topic", (arg) => {
-      const { level, ts } = arg;
-      if (increment.current === undefined) {
-        increment.current = 0;
-      }
-      if (increment.current < 8) {
-        increment.current = increment.current + 1;
-      } else {
-        removeData(dataChart);
-      }
-      addData(dataChart, ts, level);
-    });
-  }, []);
-
-  function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-      dataset.data.push(...data);
-    });
-    chart.update();
-  }
-
-  function removeData(chart) {
-    chart.data.labels.splice(-1, 1);
-    chart.data.datasets.forEach((dataset) => {
-      dataset.data.splice(0, 1);
-    });
-    chart.update();
-  }
+  socket.on("topic", (arg) => {
+    const { level } = arg;
+    currentLevel = level;
+  });
 
   const chartColors = {
     red: "rgb(255, 99, 132)",
     orange: "rgb(255, 159, 64)",
     yellow: "rgb(255, 205, 86)",
-    green: "rgb(75, 192, 192)",
-    blue: "rgb(48 128 208 / 48%)",
+    green: "rgba(44, 210, 29, 1)",
+    blue: "rgb(29, 124, 210, 100)",
     purple: "rgb(153, 102, 255)",
     grey: "rgb(201, 203, 207)",
   };
 
-  const data = {
-    datasets: [
-      {
-        label: "Wet Well Level",
-        backgroundColor: "#fff",
-        borderColor: chartColors.blue,
-        borderWidth: 3,
-        pointRadius: 3,
-        pointStyle: "circle",
-        radius: 0,
-        fill: false,
-        cubicInterpolationMode: "monotone",
-        lineTension: 0,
-      },
-    ],
-  };
 
   useEffect(() => {
     const ctx = canvas.current;
+    const gredient = canvas.current.getContext("2d");
+    let grd = gredient.createLinearGradient(0, 0, 0, 400);
+    grd.addColorStop(0, "rgba(44, 210, 29, 1)");
+    grd.addColorStop(1, "rgba(89, 89, 110, 0.5)");
     // eslint-disable-next-line react-hooks/exhaustive-deps
     dataChart = new Chart(ctx, {
       type: "line",
-      data,
-      options: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: "Suggested Min and Max Settings",
+      data: {
+        datasets: [
+          {
+            shadowOffsetX: 3,
+            shadowOffsetY: 3,
+            label: "Wet Well Level",
+            backgroundColor: grd,
+            borderColor: chartColors.blue,
+            borderWidth: 3,
+            pointRadius: 1,
+            clip: true,
+            pointStyle: "circle",
+            radius: 0,
+            fill: "start",
+            cubicInterpolationMode: "monotone",
+            lineTension: 0,
           },
-        },
-        interaction: {
-          mode: "index",
-          intersect: false,
+        ],
+      },
+      options: {
+        plugins: {
+          tooltip: {
+            enabled: true,
+            caretPadding: 10,
+            cornerRadius: 10,
+            position: 'average',
+            titleAlign: 'center',
+            callbacks: {
+              label: function (context) {
+                let label = context.dataset.label || "";
+                if (label) {
+                  label += `: ${context.parsed.y}ft`;
+                }
+                return label;
+              },
+              labelColor: function (context) {
+                return {
+                  borderColor: null,
+                  backgroundColor: "rgba(44, 210, 29, 1)",
+                  borderWidth: 2,
+                };
+              },
+              labelTextColor: function (context) {
+                return "#fff";
+              },
+            },
+          },
         },
         scales: {
           x: {
-            ticks: {
-              callback: function (value, index, values) {
-                return `${currentTime}`;
+            type: "realtime",
+            realtime: {
+              duration: 20000,
+              refresh: 1000,
+              delay: 2000,
+              onRefresh: (chart) => {
+                chart.data.datasets.forEach((dataset) => {
+                  dataset.data.push({
+                    x: Date.now(),
+                    y: currentLevel ? currentLevel :  0,
+                  });
+                });
               },
             },
           },
           y: {
+            title: {
+              display: true,
+              text: "Value",
+            },
             min: 0,
             max: 100,
-            ticks: {
-              callback: function (value, index, values) {
-                return `${value} Feet`;
-              },
-            },
           },
+        },
+        hover: {
+          mode: "index",
+          intersect: false,
+          animationDuration: 0,
+        },
+
+        interaction: {
+          intersect: false,
         },
       },
     });
-
-    dataChart.options.plugins = {
-      annotation: false,
-      datalabels: false,
-      zoom: false,
-    };
   }, []);
 
   return (
@@ -129,8 +135,6 @@ function App() {
           <canvas ref={canvas} width={600} height={400}></canvas>
         </div>
       </div>
-
-      {/* <h1>{currentLevel ? `${currentLevel[currentLevel.length - 1]}` : null}</h1> */}
     </>
   );
 }
